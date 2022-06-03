e1d9ed246b0be63f6930bc62ebbe79474a7cdb91
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2022-06-03 14:53:35 +0600

    create a model bug fixed

--- myApp.js
+++ myApp.js
@@ -16,7 +16,7 @@ const personSchema = new mongoose.Schema({
   age: Number,
   favouriteFoods: [String]
 })
-Person = mongoose.Model("Person", personSchema)
+Person = mongoose.model("Person", personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/ );
