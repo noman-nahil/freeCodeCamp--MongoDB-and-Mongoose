77a9bdb73d9a06c57b144a35c981f23645daa005
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2022-06-03 14:49:39 +0600

    create a model

--- myApp.js
+++ myApp.js
@@ -7,6 +7,17 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favouriteFoods: [String]
+})
+Person = mongoose.Model("Person", personSchema)
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/ );
 };
