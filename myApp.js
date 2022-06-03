6c837886254ee677a9cb3d68eff9c365b6c325aa
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2022-06-03 15:13:05 +0600

    create a model bug fixed-1

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-let Person;
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -16,7 +16,7 @@ let personSchema = new Schema({
   age: Number,
   favouriteFoods: [String]
 });
-Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/ );
