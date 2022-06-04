9d21b77621f14d500680cb2eac468d98e0834815
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2022-06-04 16:18:29 +0600

    Delete Many Documents with model.remove()

--- myApp.js
+++ myApp.js
@@ -125,8 +125,12 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/ );
+  Person.remove({
+    name: nameToRemove
+  }, (err, response) => {
+    if (err) return done(err);
+    done(null, response);
+  })
 };
 
 const queryChain = (done) => {
